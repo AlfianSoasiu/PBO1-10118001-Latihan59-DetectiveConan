69c366ccccbab1d287d3d165fd0af6e5004d3297
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-08 00:18:32 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan59-DetectiveConan/src/DetectiveConan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output conan
+ */
+public class DetectiveConan {
+
+    public static void main(String[] args) {
+        
+        MainCharacter conan = new MainCharacter("Investigator", "Menyelesaikan Kasus","Conan Edogawa","Tokyo",true);
+        MainCharacter ran = new MainCharacter("Karate", "Sama seperti Shinichi","Ran Mouri","Osaka",false);
+        SupportCharacter eisuke = new SupportCharacter("Teman Sekelas Ran Mouri","Eisuke Hondo","Osaka",false);
+        conan.displayCharacter();
+        ran.displayCharacter();
+        eisuke.displayCharacter();
+    }
+    
+}
